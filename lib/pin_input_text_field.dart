ee9f27be2a2669eb83805f644cb988909b27c183
Tino <guochenghaha@gmail.com>
2019-01-20 13:23:16 +0800

    allow pass keyboardType & inputFormatters. add controller to observe pin & set pin.

--- lib/pin_input_text_field.dart
+++ lib/pin_input_text_field.dart
@@ -1,5 +1,8 @@
 library pin_input_text_field;
 
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -140,31 +143,73 @@ class BoxLooseDecoration extends PinDecoration {
   PinEntryType get pinEntryType => PinEntryType.boxLoose;
 }
 
+/// Helper class to handle inner or outside controller.
+class PinEditingController extends TextEditingController {
+  /// Determine whether this class is defined inner.
+  bool _inner;
+
+  /// Control the maxLength of pin.
+  int _pinMaxLength;
+
+  PinEditingController._inner({String text, bool inner})
+      : _inner = inner,
+        super(text: text);
+
+  PinEditingController({String text})
+      : _inner = false,
+        super(text: text);
+
+  @override
+  set text(String newText) {
+    /// Cut the parameter string if the length is longer than [_pinMaxLength].
+    if (newText != null && newText.length > _pinMaxLength) {
+      newText = newText.substring(0, _pinMaxLength);
+    }
+    super.text = newText;
+  }
+}
+
 class PinInputTextField extends StatefulWidget {
+  final double width;
+
+  final double height;
+
   /// The max length of pin.
   final int pinLength;
 
   /// The callback will execute when user click done.
   final ValueChanged<String> onSubmit;
 
-  /// The callback will execute when user input.
-  final ValueChanged<String> onPinChanged;
-
-  final double width;
-
-  final double height;
-
   /// Decorate the pin.
   final PinDecoration decoration;
+
+  /// Just like [TextField]'s inputFormatter.
+  final List<TextInputFormatter> inputFormatters;
+
+  /// Just like [TextField]'s keyboardType.
+  final TextInputType keyboardType;
+
+  /// Controls the pin being edited.
+  ///
+  /// If null, this widget will create its own [PinEditingController].
+  final PinEditingController pinEditingController;
+
+//  final PinController pinController;
 
   PinInputTextField({
     this.pinLength: 6,
     this.width,
     this.height,
     this.onSubmit,
-    this.onPinChanged,
     this.decoration: const BoxLooseDecoration(),
-  }) : assert(pinLength > 0);
+    List<TextInputFormatter> inputFormatter,
+    this.keyboardType: TextInputType.phone,
+    pinEditingController,
+  })  : assert(pinLength > 0),
+        inputFormatters = inputFormatter ??
+            <TextInputFormatter>[WhitelistingTextInputFormatter.digitsOnly],
+        pinEditingController =
+            pinEditingController ?? PinEditingController._inner(inner: false);
 
   @override
   State createState() {
@@ -173,32 +218,48 @@ class PinInputTextField extends StatefulWidget {
 }
 
 class _PinInputTextFieldState extends State<PinInputTextField> {
-  final TextEditingController _controller = TextEditingController();
+  PinEditingController _controller;
   String _text;
-
-  /// The current string the user is editing.
-  String get text => _text;
 
   @override
   void initState() {
     super.initState();
+    _controller = widget.pinEditingController;
+    _controller._pinMaxLength = widget.pinLength;
+
     _controller.addListener(() {
+      /// Reset the cursor position when in needed.
+      /// Relevant issue see [https://github.com/flutter/flutter/issues/11416],
+      /// Only work in Android.
       setState(() {
-        /// Only trigger when text is really changed.
-        if (widget.onPinChanged != null &&
-            _text != null &&
-            _controller.text != null &&
-            _text.compareTo(_controller.text) != 0) {
-          widget.onPinChanged(_controller.text);
-        }
         _text = _controller.text;
+        if (Platform.isAndroid &&
+            _controller.text != null &&
+            _controller.selection.start < _controller.text.length) {
+          _controller.selection = TextSelection.fromPosition(
+            TextPosition(offset: _controller.text.length),
+          );
+        }
       });
     });
   }
 
   @override
+  void didUpdateWidget(PinInputTextField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // If the controller change from inside to outside,
+    // we should dispose the controller to prevent leak;
+    if (!_controller._inner && oldWidget.pinEditingController._inner) {
+      oldWidget.pinEditingController.dispose();
+    }
+  }
+
+  @override
   void dispose() {
-    _controller.dispose();
+    /// Only work in inner controller.
+    if (_controller._inner) {
+      _controller.dispose();
+    }
     super.dispose();
   }
 
@@ -231,20 +292,20 @@ class _PinInputTextFieldState extends State<PinInputTextField> {
           /// Disable the actual textField selection.
           enableInteractiveSelection: false,
 
-          /// The maxLength of the pin input, the default value is 6
+          /// The maxLength of the pin input, the default value is 6.
           maxLength: widget.pinLength,
 
           /// If use system keyboard and user click done, it will execute callback
           /// Note!!! Custom keyboard in Android will not execute, see the related issue [https://github.com/flutter/flutter/issues/19027]
           onSubmitted: widget.onSubmit,
 
-          /// Default text input type is number
-          keyboardType: TextInputType.numberWithOptions(signed: true),
+          /// Default text input type is number.
+          keyboardType: widget.keyboardType,
 
-          /// only accept digits
-          inputFormatters: <TextInputFormatter>[
-            WhitelistingTextInputFormatter.digitsOnly,
-          ],
+          /// only accept digits.
+          inputFormatters: widget.inputFormatters,
+
+          /// Clear default text decoration.
           decoration: InputDecoration(
             /// Hide the counterText
             counterText: '',
